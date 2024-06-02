16a7085dd13155c85d71e4d3b39ca2283c5c39b1
Martin Matyášek <martin.matyasek@gmail.com>
2024-06-03 00:08:43 +0200

    Handle missing commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
-use std::io::{self, Write as _};
+use std::io::{self, Write};
+use std::str::FromStr;
 
-fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+use anyhow::Result;
 
-    // Wait for user input
+const PROMPT: char = '$';
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("{0}: command not found")]
+    CommandNotFound(String),
+}
+
+#[derive(Debug)]
+enum Command {}
+
+impl std::fmt::Display for Command {
+    #[inline]
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
+}
+
+impl FromStr for Command {
+    type Err = Error;
+
+    fn from_str(cmd: &str) -> Result<Self, Self::Err> {
+        Err(Error::CommandNotFound(cmd.to_string()))
+    }
+}
+
+fn main() -> Result<()> {
     let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
+
+    write!(stdout, "{PROMPT} ")?;
+    stdout.flush()?;
+
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    stdin.read_line(&mut input)?;
+
+    match input.trim().parse::<Command>() {
+        Ok(cmd) => unimplemented!("handle {cmd}"),
+        Err(e) => {
+            writeln!(stderr, "{e}")?;
+            stderr.flush()?;
+        }
+    }
+
+    Ok(())
 }
